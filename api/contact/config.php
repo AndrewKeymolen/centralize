36d79ef004f8f94302c8c2c795476d8ddca379b0
Andrew Keymolen <andrewkeymolen@gmail.com>
2020-09-10 21:40:34 +0200

    moved the backend

--- api/contact/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$adminEmail =  "andrewkeymolen@gmail.com";
-$SendMailFailederrorMessage = "Sorry, something went wrong!";
-$SendNameEmptyerrorMessage = "The name field is empty!";
-$SendMailEmptyerrorMessage = "The email adress fiel is empty!";
-$SendMailInvaliderrorMessage = "The provided email is invalid!";
-$SendSubjectEmptyerrorMessage = "The subject field is empty!";
-$SendMessageEmptyerrorMessage = "The message field is empty!";
-$SendMailSuccessMessage = "Your message has been sent successfully! You should also have received a copy. Please check your SPAMS folder if you can't find it.";
